48720c138231e0e97adffa2f49eef19221c764b9
Przemek <przemek12wiewiora@gmail.com>
2020-01-24 12:23:01 +0100

    update

---
+++ min.js
@@ -0,0 +1,41 @@
+// Created by Przemysław Wiewióra
+
+function StartLoading() {
+    Show(document.getElementById('loader'), 1000);
+}
+
+function StopLoading() {
+    Hide(document.getElementById('loader'), 1000);
+}
+
+/* @element to show
+ * @time in ms 
+ * function use css opacity to show. */
+function Show(element, time) {
+    ShowSlowly(element, time, parseInt(new Date().getTime()));
+}
+
+/* Helper function. Don't call use Show(element, time) instead */
+function ShowSlowly(element, time, startTime) {
+    if (new Date().getTime() < startTime + time ){
+        let opacity = (new Date().getTime() - startTime) / time;
+        element.style.opacity = opacity;
+        setTimeout(function() { ShowSlowly(element, time, startTime) }, 25); // 25 is 40 times per sec
+    }
+}
+
+/* @element to hide
+ * @time in ms 
+ * function use css opacity to hide. */
+function Hide(element, time) {
+    HideSlowly(element, time, parseInt(new Date().getTime()));
+}
+
+/* Helper function. Don't call use Show(element, time) instead */
+function HideSlowly(element, time, startTime) {
+    if (new Date().getTime() < startTime + time ){
+        let opacity = -(new Date().getTime() - startTime - time) / time;
+        element.style.opacity = opacity;
+        setTimeout(function() { HideSlowly(element, time, startTime) }, 25); // 25 is 40 times per sec
+    }
+}
